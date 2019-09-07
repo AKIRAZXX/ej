9d778912ea943c53df5ddb6615c2506d20f7165e
AKIRAZXX <940836478@qq.com>
2019-09-07 17:27:43 +0800

    [zxx]初始化模板

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(function(){
+	// 为栏目绑定事件
+
+	$("ul.left_nav").on("click","li",function(){
+
+		var url = $(this).attr("url");
+		$("#wrapper").load(url);
+	});
+	$("li").hover(function () {
+	    $(this).attr("background:#009788");
+	});
+	$(".left_nav").on('click',"li",function(){
+        $(this).addClass("current").siblings().removeClass("current");
+    });
+	
+});
